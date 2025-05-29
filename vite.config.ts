ed1d5e0cd64a9925c90eae229d5a0753b60be3c5
xiangguge <2470713732@qq.com>
2025-05-29 22:33:20 +0800

    配置 GitHub Pages 部署：添加 base 和 deploy 脚本

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/DEEDMW/', // 关键路径配置：这个 base 是让构建后的资源路径正确适配 GitHub Pages 的子路径 /DEEDMW/。
   plugins: [
     vue(),
     vueDevTools(),
