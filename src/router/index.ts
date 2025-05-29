cf350307d19b00ab205cabc082d3a2237e428cc2
xiangguge <2470713732@qq.com>
2025-05-30 04:01:08 +0800

    fix router and vite config

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,15 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 import Graph from '@/pages/graph/graph.vue'
 import DFS_BFS from '@/pages/graph/DFS+BFS.vue'
 import Home from '@/pages/home/home.vue'
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        { path: '/', component: Home },
-        { path:'/graph', component:Graph},
-        { path:'/dfs+dfs', component:DFS_BFS},
-    ]
+  history: createWebHashHistory(),
+  routes: [
+    { path: '/', component: Home },
+    { path: '/graph', component: Graph },
+    { path: '/dfs+dfs', component: DFS_BFS },
+  ],
 })
 
 export default router
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/DEEDMW/', // 关键路径配置：这个 base 是让构建后的资源路径正确适配 GitHub Pages 的子路径 /DEEDMW/。
+  base: '/DEEDMW/#/', // 关键路径配置：这个 base 是让构建后的资源路径正确适配 GitHub Pages 的子路径 /DEEDMW/，特别是，还与路由的hash模式配合。
   plugins: [
     vue(),
     vueDevTools(),
